ff3d49c10315f0afa124aea77b2068d23c72c235
Erik Mikac <5546184+HankSchrader@users.noreply.github.com>
2023-10-13 20:05:16 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Erik Mikac on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
